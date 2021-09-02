bece6bcccd64793f02827bf210e4fb30baa842b1
Erick Kleniving <libertti@live.com>
2021-09-02 15:51:39 -0300

    KudoCard

--- kudobox/KudoController.js
+++ kudobox/KudoController.js
@@ -2,6 +2,7 @@ export default class Kudo{
   constructor(data){
     this.selectNameEl = document.getElementById('select-name');
     this.galeryEl = document.getElementById('galery');
+    this.cardSelect = document.getElementById('card-select')
     this.dataSelect = []
 
     this.data = data;
@@ -23,12 +24,14 @@ export default class Kudo{
     this.selectNameEl.addEventListener('change', e => {
       this.isActive = document.querySelectorAll('.card');
       [...this.isActive].forEach(card =>{
-        if(e.target.value ==card.dataset.nome ){
+        if(e.target.value ==card.dataset.para ){
           card.classList.add("active")
-          
+          card.classList.remove("desactve")
         }else if(e.target.value == 'all'){
           card.classList.add("active")
+          card.classList.remove("desactve")
         }else{
+          card.classList.add("desactve")
           card.classList.remove("active")
         }
       })
@@ -40,35 +43,44 @@ export default class Kudo{
 
   insertElement(selectName){ 
     this.data.forEach(card => {
-      if(!this.dataSelect.find(value => value == card.nome)){
+      if(!this.dataSelect.find(value => value == card.para)){
         this.selectNameEl.appendChild(this.createElementOption(card))
       }
       this.galeryEl.appendChild(this.insertElementCard(card))
-      this.dataSelect.push(card.nome)
+      this.dataSelect.push(card.para)
     });
 
   } // insertSelectOption
 
   createElementOption(card){
     let option = document.createElement('option')
-    option.setAttribute('value', card.de)
-    option.innerHTML = card.nome
+    option.setAttribute('value', card.para)
+    option.innerHTML = card.para
     return option;
   } // createElementOption
 
   insertElementCard(card, selectName){
     let div = document.createElement('div')
-
+    
     div.setAttribute('class','card active')
     div.setAttribute('id','card')
-    div.dataset.nome = card.para
-    div.innerHTML = `
-    <div class="card-body" style="background-image:url('./img/${this.getImg(card.type)}.png');background-size: contain;background-repeat:no-repeat;">
-			<p>De: ${card.de}</p>
-			<p>Para: ${card.para}</p>
-			<p>${card.msg}</p>
-		</div>
-    `
+    div.dataset.para = card.para
+    div.innerHTML = this.elementoCard(card)
+    div.addEventListener('click', e =>{ 
+      let div = document.createElement('div')
+      div.setAttribute('id','card')
+      div.innerHTML = this.elementoCard(card)
+    
+      this.cardSelect.classList.add('class','show')
+      this.cardSelect.innerHTML =""
+      this.cardSelect.appendChild(div)
+
+      this.cardSelect.addEventListener('click', e=>{
+        
+
+        this.cardSelect.classList.remove('show')
+      })
+    })
     return div
 
   }
@@ -96,9 +108,16 @@ export default class Kudo{
     }
     return  imagem
   }
-  getNameSelectCard(){
-
+  elementoCard(card){
+    return `
+    <div class="card-body" style="background-image:url('./img/${this.getImg(card.type)}.png');background-size: contain;background-repeat:no-repeat;">
+			<p>De: ${card.de}</p>
+			<p>Para: ${card.para}</p>
+			<p>${card.msg}</p>
+		</div>
+    `
   }
+
   
 }
 
--- kudobox/data.js
+++ kudobox/data.js
@@ -1,104 +1,87 @@
 const data = [
 	{
-		"nome":"Alisson Bolsoni (Seller)",
 		"de": "Alisson@",
 		"para": "Bruna@",
 		"type": "Obrigado",
 		"msg": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
 	},
 	{
-		"nome":"Alisson Bolsoni (Seller)",
 		"de": "Alisson@",
-		"para": "Andre@g",
+		"para": "Andre@",
 		"type": "Obrigado",
 		"msg": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
 	},
 	{
-	"nome":"André Lobo (Nets)",
 	"de": "Andre@",
 	"para": "Alisson@",
 	"type": "Ótimo trabalho",
 	"msg": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."},
 	{
-	"nome":"Bruna Baleste (Com)",
 	"de": "Bruna@",
 	"para": "Alisson@",
 	"type": "Sensacional",
 	"msg": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."},
 	{
-	"nome":"Bruno Peterson (Com)",
 	"de": "Bruno@",
 	"para": "Alisson@",
 	"type": "Parabéns",
 	"msg": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."},
 	{
-	"nome":"Bruno Yuji (Nets)",
 	"de": "BrunoYuji@",
 	"para": "Andre@",
 	"type": "Bom trabalho",
 	"msg": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."},
 	{
-	"nome":"Costelinha (Luis Araújo) [UX]",
 	"de": "Costelinha@",
 	"para": "BrunoYuji@",
 	"type": "Obrigado",
 	"msg": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."},
 	{
-	"nome":"Daniel Destro (Tribo)",
 	"de": "Daniel@",
 	"para": "David@",
 	"type": "Ótimo trabalho",
 	"msg": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."},
 	{
-	"nome":"David Garcia (Seller)",
 	"de": "David@",
 	"para": "BrunoYuji@",
 	"type": "Sensacional",
 	"msg": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."},
 	{
-	"nome":"Diego Carlos da Silva (ALO)",
 	"de": "Diego@",
 	"para": "BrunoYuji@",
 	"type": "Parabéns",
 	"msg": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."},
 	{
-	"nome":"Diego Gomes Borges (Com)",
 	"de": "DiegoGomes@",
 	"para": "Daniel@",
 	"type": "Bom trabalho",
 	"msg": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."},
 	{
-	"nome":"Edenilton de Jesus (TL)",
 	"de": "Edenilton@",
 	"para": "Daniel@",
 	"type": "Obrigado",
 	"msg": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."},
 	{
-	"nome":"Eduardo Baiense Pereira (Nets)",
 	"de": "Eduardo@",
 	"para": "Daniel@",
 	"type": "Ótimo trabalho",
 	"msg": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."},
 	{
-	"nome":"Eduardo Philippe Gimenes (ALO)",
 	"de": "DiegoGomes@",
 	"para": "Daniel@",
 	"type": "Sensacional",
 	"msg": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."},
 	{
-	"nome":"Erik Kleniving (ALO)",
 	"de": "Erik@",
 	"para": "Daniel@",
 	"type": "Parabéns",
 	"msg": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."},
 	{
-	"nome":"Gabriel Chiarelo (ALO)",
 	"de": "GabrielChiarelo@",
 	"para": "Erik@",
 	"type": "Bom trabalho",
 	"msg": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."},
 	{
-	"nome":"Gisele Morila (Tribo)",
 	"de": "Gisele@",
 	"para": "Erik@",
 	"type": "Obrigado",
